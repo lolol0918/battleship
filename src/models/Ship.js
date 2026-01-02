97de76c9fccd68cd1a92adf6986a831b29084639
Derek <derekdvalerio@gmail.com>
2026-01-02 14:28:33 +0800

    Ship factory

---
+++ src/models/Ship.js
@@ -0,0 +1,15 @@
+export default class Ship {
+  constructor(length) {
+    if (length <= 0) throw new Error('length must be positive');
+    this.length = length;
+    this.hits = 0;
+  }
+
+  hit() {
+    if (this.hits < this.length) this.hits++;
+  }
+
+  isSunk() {
+    return this.hits >= this.length;
+  }
+}
