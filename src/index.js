e4f2158d61348b6f4f1d3424976c27c18160ad3c
Derek Valerio <derekdvalerio@gmail.com>
2026-01-13 20:55:08 +0800

    Rename boardRenderer.js  renderBoard.js for clarity

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import './style.css';
+import Gameboard from './models/Gameboard.js';
+import renderBoard from './ui/renderBoard.js';
+
+const playerGameboard = new Gameboard();
+
+document.addEventListener('DOMContentLoaded', () => {
+  const placementBoard = document.getElementById('placement-board');
+  renderBoard(placementBoard, playerGameboard);
+});
