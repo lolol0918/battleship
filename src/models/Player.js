90a231429772b79b9d1cf947dd0f9f68db8527a5
Derek Valerio <derekdvalerio@gmail.com>
2026-01-14 08:59:24 +0800

    refactor: changed the gameboard property

--- src/models/Player.js
+++ src/models/Player.js
@@ -1,9 +1,7 @@
-import Gameboard from './Gameboard';
-
 export default class Player {
-  constructor(name, type = 'human') {
+  constructor(name, gameboard, type = 'human') {
     this.name = name;
     this.type = type; // 'human' or 'computer'
-    this.gameboard = new Gameboard();
+    this.gameboard = gameboard; // assign the board here
   }
 }
