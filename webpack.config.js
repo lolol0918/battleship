cdd9e64ad6d662a39d58a5edd343b4a9662e3e74
Derek Valerio <derekdvalerio@gmail.com>
2026-01-23 21:23:17 +0800

    Ready for deployment

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,21 @@
 import path from 'path';
+import { fileURLToPath } from 'url';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 
+// Fix __dirname for ES modules
+// eslint-disable-next-line no-underscore-dangle
+const __filename = fileURLToPath(import.meta.url);
+// eslint-disable-next-line no-underscore-dangle
+const __dirname = path.dirname(__filename);
+
 export default {
-  mode: 'deployment',
+  mode: 'production', // or 'development' when testing
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
     clean: true,
-    publicPath: '/battleship/',
+    publicPath: '/battleship/', // your GitHub repo name for GitHub Pages
   },
 
   devServer: {
